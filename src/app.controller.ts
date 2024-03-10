e1bacbd1e1fa8afcf0938d59210eae7cfb35f37f
Grey Boolean <greyboolean@gmail.com>
2024-03-10 20:28:10 +0530

    set up routing

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,14 @@
 import { Controller, Get } from "@nestjs/common";
 
-@Controller()
+@Controller("/app")
 export class AppController {
-	@Get()
+	@Get("/welcome")
 	getRootRoute() {
 		return "hi there!";
+	}
+
+	@Get("/bye")
+	getByeThere() {
+		return "bye there!";
 	}
 }
